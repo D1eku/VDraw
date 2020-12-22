80bc9c1168250cf25a28b9613e5bda813a8867b0
nicoolasps <64236969+nicoolasps@users.noreply.github.com>
2020-12-22 10:31:30 -0300

    dibujar figuras implementado

--- VDraw/Form1.Designer.cs
+++ VDraw/Form1.Designer.cs
@@ -30,12 +30,15 @@ namespace VDraw
         private void InitializeComponent()
         {
             this.panelFiguras = new System.Windows.Forms.Panel();
-            this.botonTriangulo = new System.Windows.Forms.Button();
-            this.buttonCircle = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.buttonRectangulo = new System.Windows.Forms.Button();
+            this.hexagonButton = new System.Windows.Forms.Button();
+            this.diamondButton = new System.Windows.Forms.Button();
+            this.labelFigurasInPanelFiguras = new System.Windows.Forms.Label();
+            this.buttonEllipse = new System.Windows.Forms.Button();
             this.buttonPentagono = new System.Windows.Forms.Button();
-            this.buttonOctagono = new System.Windows.Forms.Button();
+            this.buttonRectangulo = new System.Windows.Forms.Button();
+            this.squareButton = new System.Windows.Forms.Button();
+            this.buttonCircle = new System.Windows.Forms.Button();
+            this.botonTriangulo = new System.Windows.Forms.Button();
             this.panelCanvas = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -46,7 +49,6 @@ namespace VDraw
             this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.deshacerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.rehacerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.labelFigurasInPanelFiguras = new System.Windows.Forms.Label();
             this.panelFiguras.SuspendLayout();
             this.panel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
@@ -54,11 +56,13 @@ namespace VDraw
             // 
             // panelFiguras
             // 
+            this.panelFiguras.Controls.Add(this.hexagonButton);
+            this.panelFiguras.Controls.Add(this.diamondButton);
             this.panelFiguras.Controls.Add(this.labelFigurasInPanelFiguras);
-            this.panelFiguras.Controls.Add(this.buttonOctagono);
+            this.panelFiguras.Controls.Add(this.buttonEllipse);
             this.panelFiguras.Controls.Add(this.buttonPentagono);
             this.panelFiguras.Controls.Add(this.buttonRectangulo);
-            this.panelFiguras.Controls.Add(this.button2);
+            this.panelFiguras.Controls.Add(this.squareButton);
             this.panelFiguras.Controls.Add(this.buttonCircle);
             this.panelFiguras.Controls.Add(this.botonTriangulo);
             this.panelFiguras.Location = new System.Drawing.Point(13, 36);
@@ -66,71 +70,106 @@ namespace VDraw
             this.panelFiguras.Size = new System.Drawing.Size(200, 551);
             this.panelFiguras.TabIndex = 0;
             // 
-            // botonTriangulo
+            // hexagonButton
             // 
-            this.botonTriangulo.Location = new System.Drawing.Point(3, 76);
-            this.botonTriangulo.Name = "botonTriangulo";
-            this.botonTriangulo.Size = new System.Drawing.Size(194, 38);
-            this.botonTriangulo.TabIndex = 0;
-            this.botonTriangulo.Text = "Triangulo";
-            this.botonTriangulo.UseVisualStyleBackColor = true;
-            this.botonTriangulo.Click += new System.EventHandler(this.button1_Click);
+            this.hexagonButton.Location = new System.Drawing.Point(3, 395);
+            this.hexagonButton.Name = "hexagonButton";
+            this.hexagonButton.Size = new System.Drawing.Size(194, 38);
+            this.hexagonButton.TabIndex = 8;
+            this.hexagonButton.Text = "Hexagono";
+            this.hexagonButton.UseVisualStyleBackColor = true;
+            this.hexagonButton.Click += new System.EventHandler(this.hexagonButton_Click);
             // 
-            // buttonCircle
+            // diamondButton
             // 
-            this.buttonCircle.Location = new System.Drawing.Point(3, 120);
-            this.buttonCircle.Name = "buttonCircle";
-            this.buttonCircle.Size = new System.Drawing.Size(194, 38);
-            this.buttonCircle.TabIndex = 1;
-            this.buttonCircle.Text = "Circulo";
-            this.buttonCircle.UseVisualStyleBackColor = true;
-            this.buttonCircle.Click += new System.EventHandler(this.button1_Click_1);
+            this.diamondButton.Location = new System.Drawing.Point(3, 263);
+            this.diamondButton.Name = "diamondButton";
+            this.diamondButton.Size = new System.Drawing.Size(194, 38);
+            this.diamondButton.TabIndex = 7;
+            this.diamondButton.Text = "Rombo";
+            this.diamondButton.UseVisualStyleBackColor = true;
+            this.diamondButton.Click += new System.EventHandler(this.diamondButton_Click);
             // 
-            // button2
+            // labelFigurasInPanelFiguras
             // 
-            this.button2.Location = new System.Drawing.Point(3, 164);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(194, 38);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Cuadrado";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.labelFigurasInPanelFiguras.AutoSize = true;
+            this.labelFigurasInPanelFiguras.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold);
+            this.labelFigurasInPanelFiguras.Location = new System.Drawing.Point(3, 17);
+            this.labelFigurasInPanelFiguras.Name = "labelFigurasInPanelFiguras";
+            this.labelFigurasInPanelFiguras.Size = new System.Drawing.Size(138, 39);
+            this.labelFigurasInPanelFiguras.TabIndex = 6;
+            this.labelFigurasInPanelFiguras.Text = "Figuras";
             // 
-            // buttonRectangulo
+            // buttonEllipse
             // 
-            this.buttonRectangulo.Location = new System.Drawing.Point(3, 208);
-            this.buttonRectangulo.Name = "buttonRectangulo";
-            this.buttonRectangulo.Size = new System.Drawing.Size(194, 38);
-            this.buttonRectangulo.TabIndex = 3;
-            this.buttonRectangulo.Text = "Rectangulo";
-            this.buttonRectangulo.UseVisualStyleBackColor = true;
-            this.buttonRectangulo.Click += new System.EventHandler(this.button3_Click);
+            this.buttonEllipse.Location = new System.Drawing.Point(3, 132);
+            this.buttonEllipse.Name = "buttonEllipse";
+            this.buttonEllipse.Size = new System.Drawing.Size(194, 38);
+            this.buttonEllipse.TabIndex = 5;
+            this.buttonEllipse.Text = "Elipse";
+            this.buttonEllipse.UseVisualStyleBackColor = true;
+            this.buttonEllipse.Click += new System.EventHandler(this.buttonEllipse_Click);
             // 
             // buttonPentagono
             // 
-            this.buttonPentagono.Location = new System.Drawing.Point(3, 252);
+            this.buttonPentagono.Location = new System.Drawing.Point(3, 351);
             this.buttonPentagono.Name = "buttonPentagono";
             this.buttonPentagono.Size = new System.Drawing.Size(194, 38);
             this.buttonPentagono.TabIndex = 4;
             this.buttonPentagono.Text = "Pentagono";
             this.buttonPentagono.UseVisualStyleBackColor = true;
-            this.buttonPentagono.Click += new System.EventHandler(this.button4_Click);
+            this.buttonPentagono.Click += new System.EventHandler(this.buttonPentagono_Click);
             // 
-            // buttonOctagono
+            // buttonRectangulo
             // 
-            this.buttonOctagono.Location = new System.Drawing.Point(3, 296);
-            this.buttonOctagono.Name = "buttonOctagono";
-            this.buttonOctagono.Size = new System.Drawing.Size(194, 38);
-            this.buttonOctagono.TabIndex = 5;
-            this.buttonOctagono.Text = "Octagono";
-            this.buttonOctagono.UseVisualStyleBackColor = true;
+            this.buttonRectangulo.Location = new System.Drawing.Point(3, 219);
+            this.buttonRectangulo.Name = "buttonRectangulo";
+            this.buttonRectangulo.Size = new System.Drawing.Size(194, 38);
+            this.buttonRectangulo.TabIndex = 3;
+            this.buttonRectangulo.Text = "Rectangulo";
+            this.buttonRectangulo.UseVisualStyleBackColor = true;
+            this.buttonRectangulo.Click += new System.EventHandler(this.buttonRectangulo_Click);
+            // 
+            // squareButton
+            // 
+            this.squareButton.Location = new System.Drawing.Point(3, 176);
+            this.squareButton.Name = "squareButton";
+            this.squareButton.Size = new System.Drawing.Size(194, 38);
+            this.squareButton.TabIndex = 2;
+            this.squareButton.Text = "Cuadrado";
+            this.squareButton.UseVisualStyleBackColor = true;
+            this.squareButton.Click += new System.EventHandler(this.squareButton_Click);
+            // 
+            // buttonCircle
+            // 
+            this.buttonCircle.Location = new System.Drawing.Point(3, 88);
+            this.buttonCircle.Name = "buttonCircle";
+            this.buttonCircle.Size = new System.Drawing.Size(194, 38);
+            this.buttonCircle.TabIndex = 1;
+            this.buttonCircle.Text = "Circulo";
+            this.buttonCircle.UseVisualStyleBackColor = true;
+            this.buttonCircle.Click += new System.EventHandler(this.buttonCircle_Click);
+            // 
+            // botonTriangulo
+            // 
+            this.botonTriangulo.Location = new System.Drawing.Point(3, 307);
+            this.botonTriangulo.Name = "botonTriangulo";
+            this.botonTriangulo.Size = new System.Drawing.Size(194, 38);
+            this.botonTriangulo.TabIndex = 0;
+            this.botonTriangulo.Text = "Triangulo";
+            this.botonTriangulo.UseVisualStyleBackColor = true;
+            this.botonTriangulo.Click += new System.EventHandler(this.botonTriangulo_Click);
             // 
             // panelCanvas
             // 
+            this.panelCanvas.BackColor = System.Drawing.Color.White;
+            this.panelCanvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panelCanvas.Location = new System.Drawing.Point(221, 36);
             this.panelCanvas.Name = "panelCanvas";
             this.panelCanvas.Size = new System.Drawing.Size(747, 551);
             this.panelCanvas.TabIndex = 1;
+            this.panelCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseDown);
+            this.panelCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseUp);
             // 
             // panel1
             // 
@@ -165,20 +204,20 @@ namespace VDraw
             // asdasdToolStripMenuItem
             // 
             this.asdasdToolStripMenuItem.Name = "asdasdToolStripMenuItem";
-            this.asdasdToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.asdasdToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
             this.asdasdToolStripMenuItem.Text = "Abrir";
             this.asdasdToolStripMenuItem.Click += new System.EventHandler(this.asdasdToolStripMenuItem_Click);
             // 
             // guardarToolStripMenuItem
             // 
             this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
-            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
             this.guardarToolStripMenuItem.Text = "Guardar";
             // 
             // exportarToolStripMenuItem
             // 
             this.exportarToolStripMenuItem.Name = "exportarToolStripMenuItem";
-            this.exportarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportarToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
             this.exportarToolStripMenuItem.Text = "Exportar";
             // 
             // editarToolStripMenuItem
@@ -193,25 +232,14 @@ namespace VDraw
             // deshacerToolStripMenuItem
             // 
             this.deshacerToolStripMenuItem.Name = "deshacerToolStripMenuItem";
-            this.deshacerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deshacerToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
             this.deshacerToolStripMenuItem.Text = "Deshacer";
             // 
             // rehacerToolStripMenuItem
             // 
             this.rehacerToolStripMenuItem.Name = "rehacerToolStripMenuItem";
-            this.rehacerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rehacerToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
             this.rehacerToolStripMenuItem.Text = "Rehacer";
-            // 
-            // labelFigurasInPanelFiguras
-            // 
-            this.labelFigurasInPanelFiguras.AutoSize = true;
-            this.labelFigurasInPanelFiguras.Font = new System.Drawing.Font("Monotxt", 25F, System.Drawing.FontStyle.Bold);
-            this.labelFigurasInPanelFiguras.Location = new System.Drawing.Point(3, 17);
-            this.labelFigurasInPanelFiguras.Name = "labelFigurasInPanelFiguras";
-            this.labelFigurasInPanelFiguras.Size = new System.Drawing.Size(201, 43);
-            this.labelFigurasInPanelFiguras.TabIndex = 6;
-            this.labelFigurasInPanelFiguras.Text = "Figuras";
-            this.labelFigurasInPanelFiguras.Click += new System.EventHandler(this.label1_Click);
             // 
             // MasterUIVDraw
             // 
@@ -241,9 +269,9 @@ namespace VDraw
         private System.Windows.Forms.Button botonTriangulo;
         private System.Windows.Forms.Button buttonPentagono;
         private System.Windows.Forms.Button buttonRectangulo;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button squareButton;
         private System.Windows.Forms.Button buttonCircle;
-        private System.Windows.Forms.Button buttonOctagono;
+        private System.Windows.Forms.Button buttonEllipse;
         private System.Windows.Forms.Panel panelCanvas;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.MenuStrip menuStrip1;
@@ -255,6 +283,8 @@ namespace VDraw
         private System.Windows.Forms.ToolStripMenuItem deshacerToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem rehacerToolStripMenuItem;
         private System.Windows.Forms.Label labelFigurasInPanelFiguras;
+        private System.Windows.Forms.Button hexagonButton;
+        private System.Windows.Forms.Button diamondButton;
     }
 }
 
